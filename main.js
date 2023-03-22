633671fa857eda2fc14c84e55b331f8453c7747b
puneet2003 <puneetkumar322003@gmail.com>
2023-03-23 02:35:14 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+window.addEventListener('load',()=>{
+    const form = document.querySelector('#new-task-form');
+    const input = document.querySelector('#new-task-input');
+    const event_list = document.querySelector('#tasks');
+    // const tep = document.querySelector('#new-task-submit');
+    console.log("hlo");
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        console.log("hlonsfjkhf");
+    })
+
+})
